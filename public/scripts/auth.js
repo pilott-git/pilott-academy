8b3a18a7ed9eb40ced23f061e22b7c146dbda7f1
pilott! <147413451+pilott-git@users.noreply.github.com>
2025-02-09 14:56:50 -0500

    Create auth.js

---
+++ public/scripts/auth.js
@@ -0,0 +1,5 @@
+const backendURL = "https://pilott-academy-backend.replit.apphttps://8807e44f-1dc3-4b58-ae77-05f1c30f5abc-00-26uok01m0f3zj.riker.replit.dev/"; // Replace with your Replit backend link
+
+function loginWithDiscord() {
+    window.location.href = `${backendURL}/auth/discord`;
+}
