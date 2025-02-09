c0ab01814055928159bcc5499d9d5576cf8c2340
pilott! <147413451+pilott-git@users.noreply.github.com>
2025-02-09 14:41:45 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('loginBtn').addEventListener('click', () => {
+    window.location.href = '/auth/discord';
+});
+
+document.getElementById('subscribeBtn').addEventListener('click', () => {
+    alert("Subscription system coming soon!");
+});
