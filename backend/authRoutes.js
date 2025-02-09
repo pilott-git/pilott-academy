f43fd2d996ec73bdcaba67bd35ef571d5d9550a3
pilott! <147413451+pilott-git@users.noreply.github.com>
2025-02-09 15:03:06 -0500

    Update authRoutes.js

--- backend/authRoutes.js
+++ backend/authRoutes.js
@@ -1,1 +1,8 @@
+const express = require("express");
+const router = express.Router();
 
+router.get("/discord", (req, res) => {
+    res.send("Redirecting to Discord login...");
+});
+
+module.exports = router;
