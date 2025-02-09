ab283b18e9bfd6cdf59d6633da37e1d070db4274
pilott! <147413451+pilott-git@users.noreply.github.com>
2025-02-09 15:06:07 -0500

    Update auth.js

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,4 +1,4 @@
-const backendURL = "https://pilott-academy-backend.replit.apphttps://8807e44f-1dc3-4b58-ae77-05f1c30f5abc-00-26uok01m0f3zj.riker.replit.dev/"; // Replace with your Replit backend link
+const backendURL = "https://pilott-academy-6v841h479-pilotts-projects.vercel.app/"; // Replace with your actual Vercel link
 
 function loginWithDiscord() {
     window.location.href = `${backendURL}/auth/discord`;
